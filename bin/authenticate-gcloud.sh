9e455ea1459d4b8521c99b0b854cf6eccd27d10d
Nathan Miller <nmiller217@gmail.com>
2018-11-03 15:38:56 -0700

    update travis and fix directory for credentials

--- bin/authenticate-gcloud.sh
+++ bin/authenticate-gcloud.sh
@@ -2,11 +2,11 @@
 
 export CLOUDSDK_CORE_DISABLE_PROMPTS=1
 
-openssl aes-256-cbc -K $encrypted_0e1011058c7f_key -iv $encrypted_0e1011058c7f_iv -in credentials.tar.gz.enc -out credentials.tar.gz -d
-tar -xzf credentials.tar.gz
+openssl aes-256-cbc -K $encrypted_0e1011058c7f_key -iv $encrypted_0e1011058c7f_iv -in credentials/credentials.tar.gz.enc -out credentials/credentials.tar.gz -d
+tar -xzf credentials/credentials.tar.gz
 mkdir -p lib
 
-gcloud auth activate-service-account --key-file client-secret.json
+gcloud auth activate-service-account --key-file credentials/client-secret.json
 gcloud config set project $GCLOUD_PROJECT
 gcloud config set compute/zone $COMPUTE_ZONE
 gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin $DOCKER_REPO

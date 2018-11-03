e5fe85d08932b9a97ca6dffe0b9f74c2ae1750c4
Nathan Miller <nmiller217@gmail.com>
2018-11-03 15:45:00 -0700

    rename to push-docker.sh and move files into credengtials directory

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -20,9 +20,9 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
         source bin/install-tools.sh
         source bin/authenticate-gcloud.sh
-        source bin/docker-push.sh
+        source bin/push-docker.sh
         source bin/deploy-kube.sh
-        
+
    else
        echo "Skipping deploy because branch is not staging or production"
    fi

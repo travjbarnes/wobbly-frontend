204bb2142d74b0d003b6c78221b19d1674e5f697
Nathan Miller <nmiller217@gmail.com>
2018-11-04 10:18:26 -0800

    update deploy scripts

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -15,13 +15,17 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
         export GCLOUD_PROJECT="wobbly-app"
 
         export IMAGE_NAME="$DOCKER_IMAGE"-"$TRAVIS_BRANCH"
-        export CLUSTER_NAME="backend-cluster"-"$TRAVIS_BRANCH"
-        export DEPLOYMENT_NAME="backend-app"-"$TRAVIS_BRANCH"
+        export CLUSTER_NAME="frontend-cluster"-"$TRAVIS_BRANCH"
+        export DEPLOYMENT_NAME="frontend-app"-"$TRAVIS_BRANCH"
+        export SERVICE_NAME="frontend-app-service"-"$TRAVIS_BRANCH"
 
         source bin/install-tools.sh
         source bin/authenticate-gcloud.sh
         source bin/push-docker.sh
         source bin/deploy-kube.sh
+
+        kubectl get service $SERVICE_NAME -o json 
+        export ENDPOINT="http://0.0.0.0:8000/"
 
    else
        echo "Skipping deploy because branch is not staging or production"
---
+++ bin/test-site.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+#curl http://0.0.0.0:8000  #$ENDPOINT
